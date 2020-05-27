f8aa6e1442757bb34f5299ef0ef4aca620b5cd23
surenderpal <surenderpal134@gmail.com>
2020-05-27 17:44:53 +0530

    still not finished

--- age_app.py
+++ age_app.py
@@ -1,3 +1,9 @@
-
-
-
+# date=input('Enter  your dob in [dd|mm|yy] format:')
+# dob=date.split()
+# print(dob)
+def user_name():
+    name=input('Eunter your name:')
+    return name
+user_name()
+def greeting(name):
+    return "Hello, {}, how are you?".format(name)
