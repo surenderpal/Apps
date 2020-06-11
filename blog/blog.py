1c3133d98c13b03502b00fdf1363a75c7c971417
surenderpal <surenderpal134@gmail.com>
2020-06-11 18:27:45 +0530

    pulling data from mongodb

--- blog/blog.py
+++ blog/blog.py
@@ -1,8 +1,10 @@
 import pymongo
+import post
+import sys
 uri="mongodb://127.0.0.1:27017"
 client= pymongo.MongoClient(uri)
 database=client['fullstack']
 collection= database['students']
-students=collection.find({})
-for student in students:
-    print(students)
+students=[student['name'] for student in collection.find({})]
+# print(students)
+sys.module
