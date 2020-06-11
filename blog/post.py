60e7ddcd8e0f3f21b346e48a614cde6bed0cfefb
surenderpal <surenderpal134@gmail.com>
2020-06-11 18:26:51 +0530

    added post file

---
+++ blog/post.py
@@ -0,0 +1,10 @@
+class Post(object):
+
+    def __init__(self):
+        self.title='This is title'
+        self.content='This is content'
+        self.author='Surender Pal'
+post=Post()
+print(post.content)
+print(post.author)
+print(post.title)
